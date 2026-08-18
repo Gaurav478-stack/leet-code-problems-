28e05f1046b3ce43a527a96e8b2194abe4c86613
Gaurav478-stack <2410030478@klh.edu.in>
2026-08-18 20:59:32 +0530

    add solution for 709 problem

---
+++ 709_to_lower_case.cpp
@@ -0,0 +1,13 @@
+class solution {
+public:
+    string toLowerCase(string s) {
+           for (char &c : s) {
+             if (c >= 'A' && c<= 'Z') {
+                c += 32;
+             }
+         }
+      
+         return s;
+     }
+
+};
